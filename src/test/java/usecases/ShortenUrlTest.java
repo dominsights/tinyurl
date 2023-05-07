8503b37b77c40664abb5a32044b32337cb6abc45
Domício Medeiros <domicio.medeiros@proton.me>
2023-05-07 14:25:21 +0100

    project setup

---
+++ src/test/java/usecases/ShortenUrlTest.java
@@ -0,0 +1,15 @@
+package usecases;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ShortenUrlTest {
+    @Test
+    @DisplayName("shorten given url")
+    void shortenGivenUrl() {
+        String url = "https://www.amazon.es/-/pt/dp/B07MSN1NPC/ref=pd_ci_mcx_mh_mcx_views_0?pd_rd_w=J8QXY&content-id=amzn1.sym.a34645c3-e1d7-498e-bcb7-a548b6540162&pf_rd_p=a34645c3-e1d7-498e-bcb7-a548b6540162&pf_rd_r=Z4FD07F2T1MA1RVWMFM2&pd_rd_wg=MKVXQ&pd_rd_r=35d5f0fb-4ae7-4c99-8f21-a21feb43885f&pd_rd_i=B07MSN1NPC";
+        fail("Not implemented");
+    }
+}
